fced15f1ea7d37c9528f6c8c34f9444bfb502f3d
Adwait Arajaria <adeveloper121@gmail.com>
2019-11-15 23:37:42 +0530

    No more commits to the level editor
    Almost destroyed the whole project using github. Final commit to Level Editor, I am creating a new branch.

--- colorox/Assets/_PROJECT/_SCRIPTS/Generator.cs
+++ colorox/Assets/_PROJECT/_SCRIPTS/Generator.cs
@@ -6,19 +6,32 @@ using UnityEngine;
 public class Generator : MonoBehaviour {
     
     StoredEnergy energyComponent;
+    float sliderChange;
 
     private void Start()
     {
         energyComponent = GetComponent<StoredEnergy>();
     }
-
-    private void OnMouseDown()
+    //Calculate the slider value change, then shoot if change < 0.1 and don't shoot if more than 0.1. This is the way to solve the slider problem.
+    private void OnMouseUp()
     {
-        if (energyComponent.currentEnergy > 0 && energyComponent.canShootAgain)
+        if(sliderChange < 0.05f)
         {
-            GameObject energy = (GameObject)Instantiate(Resources.Load("Energy"), transform.position, Quaternion.identity);
-            energy.GetComponent<Energy>().SetEnergy(energyComponent.energyToReduce, energyComponent.GetColor(), gameObject);
+            if (energyComponent.currentEnergy > 0 && energyComponent.canShootAgain)
+            {
+                GameObject energy = (GameObject)Instantiate(Resources.Load("Energy"), transform.position, Quaternion.identity);
+                energy.GetComponent<Energy>().SetEnergy(energyComponent.energyToReduce, energyComponent.GetColor(), gameObject);
+            }
+            energyComponent.ReduceEnergy();
         }
-        energyComponent.ReduceEnergy();
+        if (GetComponentInParent<WattSlider>())
+        {
+            GetComponentInParent<WattSlider>().totalChange = 0;
+        }
+    }
+
+    public void GetSliderChange(float change)
+    {
+        sliderChange = change;
     }
 }
---
+++ colorox/Assets/_PROJECT/_SCRIPTS/WattSlider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WattSlider : MonoBehaviour {
+
+    [HideInInspector]
+    public float totalChange;
+
+    private Slider slider;
+    
+    float oldValue;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        oldValue = slider.value;
+    }
+
+    public void CalculateChange()
+    {
+        float currentValue = slider.value;
+        float change = Mathf.Abs(currentValue - oldValue);
+        oldValue = currentValue;
+
+        totalChange += change;
+        GetComponentInChildren<Generator>().GetSliderChange(totalChange);
+    }
+}
